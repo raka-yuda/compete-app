b7bc6cbea317a7a874fe10cc8ea1bc3d7f6a021a
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2023-01-19 19:17:27 +0700

    Update param env

--- config/index.js
+++ config/index.js
@@ -7,6 +7,6 @@ module.exports = {
     rootPath: path.resolve(__dirname, '..'),
     serviceName: process.env.SERVICE_NAME,
     jwtKey: process.env.SECRET,
-    urlDb: process.env.MONGO_URL,
+    urlDb: process.env.MONGODB_URI,
     sessionKey: process.env.SESSION_SECRET
 }
